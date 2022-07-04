b2d3a4f477cb361c2209d39ddd99ddea560adec0
ezgi.duz <ezgi.duz@NB-0176>
2022-07-05 00:00:17 +0300

    Add project files.

---
+++ GM_PCB_Current_Test/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace GM_PCB_Current_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateConnection = new System.Windows.Forms.Button();
+            this.txtPowerSupplyAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCommandLine = new System.Windows.Forms.TextBox();
+            this.btnRunCommand = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtxtEquipmentResponse = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreateConnection
+            // 
+            this.btnCreateConnection.Location = new System.Drawing.Point(792, 81);
+            this.btnCreateConnection.Name = "btnCreateConnection";
+            this.btnCreateConnection.Size = new System.Drawing.Size(167, 40);
+            this.btnCreateConnection.TabIndex = 0;
+            this.btnCreateConnection.Text = "Bağlantı Kur";
+            this.btnCreateConnection.UseVisualStyleBackColor = true;
+            this.btnCreateConnection.Click += new System.EventHandler(this.btnCreateConnection_Click);
+            // 
+            // txtPowerSupplyAddress
+            // 
+            this.txtPowerSupplyAddress.Font = new System.Drawing.Font("Montserrat", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPowerSupplyAddress.Location = new System.Drawing.Point(316, 81);
+            this.txtPowerSupplyAddress.Name = "txtPowerSupplyAddress";
+            this.txtPowerSupplyAddress.Size = new System.Drawing.Size(470, 36);
+            this.txtPowerSupplyAddress.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Güç Kaynağı Adresi : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(210, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(526, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GM PCB Akım Test Programı";
+            // 
+            // txtCommandLine
+            // 
+            this.txtCommandLine.Font = new System.Drawing.Font("Montserrat", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCommandLine.Location = new System.Drawing.Point(316, 139);
+            this.txtCommandLine.Name = "txtCommandLine";
+            this.txtCommandLine.Size = new System.Drawing.Size(470, 36);
+            this.txtCommandLine.TabIndex = 4;
+            // 
+            // btnRunCommand
+            // 
+            this.btnRunCommand.Location = new System.Drawing.Point(792, 139);
+            this.btnRunCommand.Name = "btnRunCommand";
+            this.btnRunCommand.Size = new System.Drawing.Size(167, 40);
+            this.btnRunCommand.TabIndex = 5;
+            this.btnRunCommand.Text = "Çalıştır";
+            this.btnRunCommand.UseVisualStyleBackColor = true;
+            this.btnRunCommand.Click += new System.EventHandler(this.btnRunCommand_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "VISA Komutu : ";
+            // 
+            // rtxtEquipmentResponse
+            // 
+            this.rtxtEquipmentResponse.Location = new System.Drawing.Point(316, 198);
+            this.rtxtEquipmentResponse.Name = "rtxtEquipmentResponse";
+            this.rtxtEquipmentResponse.Size = new System.Drawing.Size(470, 240);
+            this.rtxtEquipmentResponse.TabIndex = 7;
+            this.rtxtEquipmentResponse.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ekipman Cevabı : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 532);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rtxtEquipmentResponse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRunCommand);
+            this.Controls.Add(this.txtCommandLine);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPowerSupplyAddress);
+            this.Controls.Add(this.btnCreateConnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateConnection;
+        private System.Windows.Forms.TextBox txtPowerSupplyAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCommandLine;
+        private System.Windows.Forms.Button btnRunCommand;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtxtEquipmentResponse;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GM_PCB_Current_Test/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NationalInstruments.VisaNS;
+
+namespace GM_PCB_Current_Test
+{
+    public partial class Form1 : Form
+    {
+
+        public static string sAddress="";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private static void InitializeProject()
+        {
+            sAddress = "GPIB0::10::INSTR";
+
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPowerSupplyAddress.Text = "GPIB0::10::INSTR";
+            InitializeProject();
+            
+        }
+
+        private void btnCreateConnection_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sAddress = txtPowerSupplyAddress.Text;
+                //The VNA uses a message based session
+                MessageBasedSession mbSession = null;
+                //But we'll just open a generic Session first
+                Session mySession = null;
+
+                //response string
+                string responseString = null;
+                //open a Session to the VNA
+                mySession = ResourceManager.GetLocalManager().Open(sAddress);
+                //cast this to a message based session
+                mbSession = (MessageBasedSession)mySession;
+
+                if(mbSession==null)
+                {
+                    MessageBox.Show("Bağlantı kurulamadı");
+                }
+                
+                //Send "*IDN?" command
+                mbSession.Write("*IDN?\n");
+                //Notice that the Session Write commands are terminated with a “/n”.
+                //This is the newline character and is not really needed for GPIB,
+                //USB or VXI-11, but is needed for Sockets.
+                //It doesn’t cause any problems to include it.
+                //Read the response
+                responseString = mbSession.ReadString();
+
+                //Write to Console
+                Console.WriteLine("Response to *IDN?:");
+
+                MessageBox.Show("Bağlantı başarılı bir şekilde kuruldu"+"\nBağlantı kurulan cihaz\n"+responseString);
+                Console.WriteLine(responseString);
+                //Return to Local Control
+                mbSession.Write("RTL\n");
+                //Close the Session
+                mbSession.Dispose();
+
+            }
+            catch (VisaException v_exp)
+            {
+                Console.WriteLine("Visa caught an error!!");
+                Console.WriteLine(v_exp.Message);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Something didn't work!!");
+                Console.WriteLine(exp.Message);
+                Console.WriteLine();
+            }
+        }
+
+        private void btnRunCommand_Click(object sender, EventArgs e)
+        {
+            if(txtCommandLine.Text==String.Empty)
+            {
+                MessageBox.Show("Komut boş olamaz");
+            }
+            else
+            {
+                try
+                {
+
+                    rtxtEquipmentResponse.Clear();
+
+
+                    //The VNA uses a message based session
+                    MessageBasedSession mbSession = null;
+                    //But we'll just open a generic Session first
+                    Session mySession = null;
+
+                    //response string
+                    string responseString = null;
+                    //open a Session to the VNA
+                    mySession = ResourceManager.GetLocalManager().Open(sAddress);
+                    //cast this to a message based session
+                    mbSession = (MessageBasedSession)mySession;
+
+                    if (mbSession == null)
+                    {
+                        MessageBox.Show("Bağlantı kurulamadı");
+                    }
+                    rtxtEquipmentResponse.Text = "Komut gönderme başarılı\n";
+                    //Send "*IDN?" command
+                    mbSession.Write(txtCommandLine.Text + "\n");
+                    //Notice that the Session Write commands are terminated with a “/n”.
+                    //This is the newline character and is not really needed for GPIB,
+                    //USB or VXI-11, but is needed for Sockets.
+                    //It doesn’t cause any problems to include it.
+                    //Read the response
+                    responseString = mbSession.ReadString();
+                    rtxtEquipmentResponse.AppendText("\nGönderilen komut : " + txtCommandLine.Text);
+                    rtxtEquipmentResponse.AppendText("\n\nEkipmandan alınan cevap :\n");
+                    rtxtEquipmentResponse.AppendText("\n" + responseString);
+                    //Return to Local Control
+                    //mbSession.Write("*RTL?\n");
+                    //Close the Session
+                    mbSession.Dispose();
+
+                }
+                catch (VisaException v_exp)
+                {
+                    Console.WriteLine("Visa caught an error!!");
+                    Console.WriteLine(v_exp.Message);
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine("Something didn't work!!");
+                    Console.WriteLine(exp.Message);
+                    Console.WriteLine();
+                }
+            }
+
+            
+        }
+    }
+
+    
+}
